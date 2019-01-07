7d003c8ce83bee3fb325d5396454ed4d63fe3e23
nikrb <nik_rb@yahoo.com>
2019-01-07 11:38:15 +0000

    test: add AnswerBox dragStart

---
+++ src/AnswerBox.test.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { cleanup, fireEvent, render } from 'react-testing-library';
+import 'jest-styled-components';
+
+import AnswerBox from './AnswerBox';
+
+afterEach(() => cleanup());
+
+it('should mark answers as draggable', () => {
+  const { getByTestId } = render(<AnswerBox answers={['brown']} />);
+  const answer = getByTestId('answer');
+  expect(answer.textContent).toBe('brown');
+});
+
+it('should call setData', () => {
+  const { getByTestId } = render(<AnswerBox answers={['brown']} />);
+  const answer = getByTestId('answer');
+  const mockdt = { setData: jest.fn() };
+  fireEvent.dragStart(answer, { dataTransfer: mockdt});
+  expect(mockdt.setData).toBeCalled();
+});
